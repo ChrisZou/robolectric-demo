2f3b8cbe56a88a77763306510782639d66f519c5
Zou,Yong <zoumengdie@gmail.com>
2015-06-14 16:45:54 +0800

    Remove CircularProgressView stuff

--- app/src/main/java/com/chriszou/testrobolectric/MainActivity.java
+++ app/src/main/java/com/chriszou/testrobolectric/MainActivity.java
@@ -2,12 +2,10 @@ package com.chriszou.testrobolectric;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,33 +21,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
-
-        testCircularProgressView();
     }
 
-
-    private boolean runed;
-    private void testCircularProgressView() {
-    //        CircularProgressView progressView = (CircularProgressView) findViewById(R.id.progress_view);
-//        System.out.println("oncreate");
-//        try {
-////            progressView.startAnimation();
-//        } catch (Exception e) {
-//            System.out.println("exception happened when start animation");
-//            e.printStackTrace();
-//        }
-//        System.out.println("oncreate end");
-////        testPostRunnable();
-    }
-
-    private void testPostRunnable() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                runed = true;
-                System.out.println("post delayed");
-                Toast.makeText(MainActivity.this, "post delayed", Toast.LENGTH_SHORT).show();
-            }
-        }, 2000);
-    }
 }
