4522cb5f550d0440cdf231301dca31e050cdf272
Zou,Yong <zoumengdie@gmail.com>
2015-06-14 17:39:00 +0800

    Add a second activity and test MainActivity can open second activity successfully.

--- app/src/main/java/com/chriszou/testrobolectric/SecondActivity.java
+++ app/src/main/java/com/chriszou/testrobolectric/SecondActivity.java
@@ -1,8 +1,15 @@
 package com.chriszou.testrobolectric;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by Chris on 6/14/15.
  */
-public class SecondActivity extends Activity{}
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
--- app/src/test/java/com/chriszou/testrobolectric/MainActivityTest.java
+++ app/src/test/java/com/chriszou/testrobolectric/MainActivityTest.java
@@ -1,12 +1,16 @@
 package com.chriszou.testrobolectric;
 
+import android.content.Intent;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.Shadows;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
 
 /**
  * Created by Chris on 6/12/15.
@@ -17,6 +21,11 @@ public class MainActivityTest {
     @Test
     public void testMainActivity() {
         MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
-        Assert.assertNotNull(mainActivity);
+        mainActivity.findViewById(R.id.textView1).performClick();
+        
+        Intent expectedIntent = new Intent(mainActivity, SecondActivity.class);
+        ShadowActivity shadowActivity = Shadows.shadowOf(mainActivity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        Assert.assertEquals(expectedIntent, actualIntent);
     }
 }
